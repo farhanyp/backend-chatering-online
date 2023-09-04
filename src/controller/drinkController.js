b8d106cc7cae3c1e166f9bd58a067e6838876b38
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-09-04 14:03:47 +0700

    fix drink get

--- src/controller/drinkController.js
+++ src/controller/drinkController.js
@@ -33,6 +33,18 @@ const get = async (req, res, next) => {
 
         const result = await drinkService.get()
 
+        const resultCopy = JSON.parse(JSON.stringify(result));
+
+        function bufferToBase64(buffer) {
+            return Buffer.from(buffer).toString('base64');
+          }
+
+          for (const data of resultCopy) {
+            if (data && data.dataImage) {
+              data.dataImage = bufferToBase64(data.dataImage);
+            }
+          }
+
         res.status(200).json({
             data: result
         })
