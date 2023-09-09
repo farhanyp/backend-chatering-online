a33bb6a9b82ee58e207eb781037edb0497b0c27b
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-09-09 20:48:57 +0700

    fix user

--- src/model/User.js
+++ src/model/User.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    number:{
+    phone:{
         type: String,
         required: true
     },
--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -9,8 +9,8 @@ const registerValidation = Joi.object({
     username: Joi.string().max(100).required(),
     password: Joi.string().max(100).required(),
     name: Joi.string().max(100).required(),
-    alamat: Joi.string().min(1).required(),
-    nohp: Joi.string().min(1).required()
+    address: Joi.string().min(1).required(),
+    phone: Joi.string().min(1).required()
 })
 
 export{
