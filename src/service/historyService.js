404a050efa324fe8f469a0bea383a521c15d13d3
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-09-16 22:58:03 +0700

    fix history

--- src/service/historyService.js
+++ src/service/historyService.js
@@ -8,30 +8,14 @@ const get = async (user)=>{
 
     const UserFind = await User.findOne({_id: user._id})
 
-    let data = {}
-
     if(UserFind){
         const UserFindForRelations = UserFind.relations.map(history => history)
         const  histories = await RelationHistory.find({_id: UserFindForRelations})
             .populate('history')
-
-        data.history = histories
-
-        const foodForRelations = histories.map( history => history.history.foodId)
-        if(foodForRelations[0].length == 0){
-            const food = await Food.findOne({_id: foodForRelations})
-            data.food = food
-        }
-
-        const drinkForRelations = histories.map( history => history.history.drinkId)
-        if(drinkForRelations[0].length === 0){
-            const drink = await Drink.findOne({_id: drinkForRelations})
-            data.drink = drink
-        }
-
+        
+        return histories
     }
 
-    return data
 }
 
 export default{
